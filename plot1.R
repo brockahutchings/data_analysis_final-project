aadc8ef1a9400f2688a2eef7112a380aab722e09
Brock Hutchings <brockahutchings@gmail.com>
2017-04-08 16:00:09 -0400

    add plot1 png and R files

--- plot1.R
+++ plot1.R
@@ -6,5 +6,22 @@
 
 #Import the files as NEI and SCC
 NEI <- readRDS("summarySCC_PM25.rds")
+
 SCC <- readRDS("Source_Classification_Code.rds")
+
+#get Sample data
+NEIsample <- NEI[sample(nrow(NEI), size = 1000, replace = F), ]
+
+#set aggrigates
+Emissions <- aggregate(NEI[, 'Emissions'], by = list(NEI$year), FUN = sum)
+
+#round emmisions
+Emissions$PM <- round(Emissions[, 2] / 1000, 2)
+
+#print plot 1
+png(filename = "plot1.png")
+barplot(Emissions$PM, names.arg = Emissions$Group.1, main = expression('Total Emission of PM'[2.5]), xlab = 'Year', ylab = expression(paste('PM', ''[2.5], ' in Kilotons')))
+
+#turn dev off
+dev.off()
 
