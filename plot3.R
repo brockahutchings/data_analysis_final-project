f4945fda2241acb985bc8a739729f658dbbd7a1e
Brock Hutchings <brockahutchings@gmail.com>
2017-04-08 16:16:17 -0400

    add plot3.R and plot3.png

--- plot3.R
+++ plot3.R
@@ -9,3 +9,27 @@
 #Import the files as NEI and SCC
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+
+#get sample data
+NEIsample <- NEI[sample(nrow(NEI), size = 5000, replace = F), ]
+
+#get subset of fips
+MD <- subset(NEI, fips == 24510)
+
+#limit years
+MD$year <- factor(MD$year, levels = c('1999', '2002', '2005', '2008'))
+
+#setting for output
+png('plot3.png', width = 800, height = 500, units = 'px')
+
+#generate plot
+ggplot(data = MD, aes(x = year, y = log(Emissions))) +
+  facet_grid(. ~ type) + guides(fill = F) +
+  geom_boxplot(aes(fill = type)) + stat_boxplot(geom = 'errorbar') +
+  ylab(expression(paste('Log', ' of PM'[2.5],
+                        ' Emissions'))) + xlab('Year') +
+                        ggtitle('Baltimore City, Maryland Emissions per Type ')+
+                        geom_jitter(alpha = 0.10)
+
+#remember to turn dev off
+dev.off()
