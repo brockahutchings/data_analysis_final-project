48da70eadcacafc149d356ef142623a6333c03e6
Brock Hutchings <brockahutchings@gmail.com>
2017-04-08 16:27:28 -0400

    add plots 4 5 & 6 (R & png

--- plot5.R
+++ plot5.R
@@ -3,6 +3,28 @@
 #in Baltimore City?
 ##########################################
 
-#Import the files as NEI and SCC
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
+#Import the file as NEI and SCC
+NEI <- readRDS("data/summarySCC_PM25.rds")
+
+#limit years
+NEI$year <- factor(NEI$year, levels = c('1999', '2002', '2005', '2008'))
+
+##SCC
+SCC <- readRDS("data/Source_Classification_Code.rds")
+
+# Baltimore City fips
+MD.onroad <- subset(NEI, fips == 24510 & type == 'ON-ROAD')
+
+# Aggregate
+MD.df <- aggregate(MD.onroad[, 'Emissions'], by = list(MD.onroad$year), sum)
+
+colnames(MD.df) <- c('year', 'Emissions')
+
+#set file name
+png('plot5.png')
+
+#generate plot
+ggplot(data = MD.df, aes(x = year, y = Emissions)) + geom_bar(aes(fill = year), stat = "identity") + guides(fill = F) + ggtitle('Total Emissions of Motor Vehicle Sources in Baltimore City, Maryland') + ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position = 'none') + geom_text(aes(label = round(Emissions, 0), size = 1, hjust = 0.5, vjust = 2))
+
+#remember to turn dev off
+dev.off()
--- plot6.R
+++ plot6.R
@@ -5,6 +5,43 @@
 #vehicle emissions?
 #########################################
 
-#Import the files as NEI and SCC
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
+#Import NEI
+NEI <- readRDS("data/summarySCC_PM25.rds")
+
+#limit the years of NEI
+NEI$year <- factor(NEI$year, levels = c('1999', '2002', '2005', '2008'))
+
+#Import SCC
+SCC <- readRDS("data/Source_Classification_Code.rds")
+
+#calculate flips for both cities
+MD.onroad <- subset(NEI, fips == '24510' & type == 'ON-ROAD')
+
+CA.onroad <- subset(NEI, fips == '06037' & type == 'ON-ROAD')
+
+# Aggregate
+MD.DF <- aggregate(MD.onroad[, 'Emissions'], by = list(MD.onroad$year), sum)
+
+#set col names
+colnames(MD.DF) <- c('year', 'Emissions')
+
+MD.DF$City <- paste(rep('MD', 4))
+
+CA.DF <- aggregate(CA.onroad[, 'Emissions'], by = list(CA.onroad$year), sum)
+
+#set col names
+colnames(CA.DF) <- c('year', 'Emissions')
+
+CA.DF$City <- paste(rep('CA', 4))
+
+#create dataframe
+DF <- as.data.frame(rbind(MD.DF, CA.DF))
+
+#name plot
+png('plot6.png')
+
+#generate plot
+ggplot(data = DF, aes(x = year, y = Emissions)) + geom_bar(aes(fill = year),stat = "identity") + guides(fill = F) + ggtitle('Total Emissions of Motor Vehicle Sources\nLos Angeles County, California vs. Baltimore City, Maryland') + ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position = 'none') + facet_grid(. ~ City) + geom_text(aes(label = round(Emissions, 0), size = 1, hjust = 0.5, vjust = -1))
+
+#remember to turn dev off
+dev.off()
